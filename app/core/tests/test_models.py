95c5559f1e1aff6905e9e3769177c25a96be041f
Cristian Favaro <cristianfavaro@cristian.local>
2020-09-20 11:23:30 -0300

    created ingredient endpoint

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -53,3 +53,11 @@ class ModelTests(TestCase):
             name='Vegan',
         )
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucamber',
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
