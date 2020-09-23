67502c3677ecc958480eb9a1b15a55fb87798642
Cristian Favaro <cristianfavaro@cristian.local>
2020-09-22 23:02:05 -0300

    added recipe image upload featured

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 from core import models
 
 
+
 def sample_user(email='test@cris.com', password='123456'):
     """Create a sample user"""
     return get_user_model().objects.create_user(email, password)
